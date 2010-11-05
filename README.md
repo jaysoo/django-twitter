5af57f82d9217555932cd0a894c26103b2655b81
Jack Hsu <jack@kazuma.(none)>
2010-11-05 01:23:05 +0000

    Added license, readme, and ignore *.swp

---
+++ README.md
@@ -0,0 +1,4 @@
+Django-Tweet
+============
+
+To be completed... ;)
--- setup.py
+++ setup.py
@@ -1,13 +1,47 @@
 from setuptools import setup, find_packages
 
+__author__ = 'jack.hsu@gmail.com'
+__version__ = '0.1-devel'
+
 setup(
-    name = "django-tweet",
-    version = "0.1",
+    name = 'django-tweet',
+    version =__version__,
+    py_modules = ['django_tweet'],
     url = 'http://github.com/jaysoo/django-tweet',
     license = 'MIT',
     description ='Manages twitter accounts a for Django project',
-    author = 'Jack Hsu',
+    author='Jack Hsu',
+    author_email='jack.hsu@gmail.com'
     packages = find_packages('django_tweet'),
     package_dir = {'': 'django_tweet'},
-    install_requires = ['setuptools'],
+    license='The MIT License',
+    keywords='twitter django oauth api',
 )
+
+SETUPTOOLS_METADATA = dict(
+    install_requires = ['setuptools', 'twitter'],
+    include_package_data = True,
+)
+
+def Read(file):
+    return open(file).read()
+
+def BuildLongDescription():
+    return '\n'.join([Read('LICENSE'), Read('README.md')])
+
+def Main():
+    # Build the long_description from the README and CHANGES
+    METADATA['long_description'] = BuildLongDescription()
+
+    # Use setuptools if available, otherwise fallback and use distutils
+    try:
+        import setuptools
+        METADATA.update(SETUPTOOLS_METADATA)
+        setuptools.setup(**METADATA)
+    except ImportError:
+        import distutils.core
+        distutils.core.setup(**METADATA)
+
+
+if __name__ == '__main__':
+    Main()
