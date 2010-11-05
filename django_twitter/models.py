e3e4ce6d6f3060f126176be3c9f84703a65f01d1
Jack Hsu <jhsu@Jack-Hsu.local>
2010-11-05 10:20:08 -0400

    Added function for TwitterAccount to post updateds

--- django_twitter/models.py
+++ django_twitter/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
+from django.conf import settings
+
+import twitter
 
 class TwitterAccount(models.Model):
     username = models.CharField(_("username"), max_length=200, unique=True, null=False, blank=False)
@@ -9,6 +12,16 @@ class TwitterAccount(models.Model):
     def __unicode__(self):
         return self.username
     
+    def post_status_update(self):
+        try:
+            api = twitter.Api(consumer_key=settings.TWITTER_CONSUMER_KEY,
+                    consumer_secret=settings.TWITTER_CONSUMER_SECRET,
+                    access_token_key=self.access_token_key,
+                    access_token_secret=self.access_token_secret)
+            return api.PostUpdate(mesg)
+        except:
+           return False
+
     class Meta:
         verbose_name = _("twitter account")
         verbose_name_plural = _("twitter accounts")
